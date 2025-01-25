5a96e41db6292c41599f24be76b34ed0a171e488
MakArtKar <makoyan2001@gmail.com>
2025-01-25 01:26:40 +0100

    ABC case MVP

--- telegram/main.py
+++ telegram/main.py
@@ -1,14 +1,16 @@
+import json
 import os
 
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-
 from dotenv import load_dotenv
+import httpx
+
 
 load_dotenv()
 TG_API_TOKEN = os.getenv("TG_API_TOKEN")
-# SIMULATION_CORE_HOST = os.getenv('SIMULATION_CORE_HOST')
+SIMULATION_CORE_HOST = os.getenv("SIMULATION_CORE_HOST", "http://localhost:8001")
 HTTP_TIMEOUT = 1200
 
 bot = Bot(token=TG_API_TOKEN)
@@ -22,17 +24,58 @@ async def send_welcome(message: types.Message):
 
 @dp.message(Command("create_simulation"))
 async def create_simulation(message: types.Message):
-    await message.reply("TODO: add simulation creation")
+    response = httpx.post(
+        os.path.join(SIMULATION_CORE_HOST, "create_simulation"),
+        timeout=HTTP_TIMEOUT,
+        json={"id": str(message.chat.id)},
+    )
+
+    if response.status_code == 200:
+        if response.text == "null":
+            await bot.send_message(
+                message.chat.id,
+                "You already started negotiations. If you want to stop them - send /end_simulation",
+            )
+            return
+        for text in json.loads(response.text):
+            await bot.send_message(message.chat.id, text)
+    else:
+        await message.answer("Something went wrong...")
 
 
 @dp.message(Command("end_simulation"))
 async def end_simulation(message: types.Message):
-    await message.reply("TODO: add simulation ending")
+    response = httpx.post(
+        os.path.join(SIMULATION_CORE_HOST, "end_simulation"),
+        timeout=HTTP_TIMEOUT,
+        json={"id": str(message.chat.id)},
+    )
+
+    if response.status_code == 200:
+        await bot.send_message(message.chat.id, "Negotiations are ended")
+    else:
+        await message.answer("Something went wrong...")
 
 
-@dp.message(Command("message_to_agent"))
+@dp.message()
 async def message_to_agent(message: types.Message):
-    await message.reply("TODO: add ability to send the message")
+    response = httpx.post(
+        os.path.join(SIMULATION_CORE_HOST, "get_text"),
+        timeout=HTTP_TIMEOUT,
+        json={"id": str(message.chat.id), "message": message.text},
+    )
+
+    if response.status_code == 200:
+        if response.text == "null":
+            await bot.send_message(
+                message.chat.id,
+                "These negotiations are ended, please, send /end_simulation",
+            )
+            return
+        for text in json.loads(response.text):
+            await bot.send_message(message.chat.id, text)
+    else:
+        await message.answer("Something went wrong...")
 
 
 async def main():
